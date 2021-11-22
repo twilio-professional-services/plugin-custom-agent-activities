4c0436520993e6bdce18fcaa98263c59f0239b68
Lindsay Hamoudi <lhamoudi@twilio.com>
2021-11-22 13:30:39 -0800

    First commit to fork - subbing out the use of Flex Config for simpler constants

--- src/utils/AgentActivities/index.js
+++ src/utils/AgentActivities/index.js
@@ -1,11 +1,15 @@
 import * as Flex from '@twilio/flex-ui';
+import { ActivitySettings } from '../../enums';
 
 class AgentActivities {
 
   constructor() {
     this.manager = Flex.Manager.getInstance();
-    const { agentActivityRules } = this.manager.serviceConfiguration.ui_attributes;
-    this.config = agentActivityRules;
+    // const { agentActivityRules } = this.manager.serviceConfiguration.ui_attributes;
+    // this.config = agentActivityRules;
+
+    // Alternative implementation from using Flex Configuration object - use constants
+    this.config = new Map(Object.entries(ActivitySettings));
   }
 
   getCSSConfig() {
@@ -14,13 +18,13 @@ class AgentActivities {
     const { routing = { skills: [], levels: {} } } = attributes;
     const skills = routing.skills || [];
 
-    // NOTE: This will hide any TR activities that are NOT set in the flex config
-    //       So make sure the deployed flex config contains all activities you'd like to appear in this menu
+    // NOTE: This will hide any TR activities that are NOT set in the config or for which 
+    // the agent is not skilled
     return Array.from(activities.values()).reduce((results, activity, idx) => {
       let cssConfig = { idx, display: 'none', order: idx };
-      const activityRule = this.config[activity.sid];
-      if (activityRule) {
-        const { requiredSkill, sortOrder } = activityRule;
+      const activitySetting = this.config.get(activity.name);
+      if (activitySetting) {
+        const { requiredSkill, sortOrder } = activitySetting;
         if (!requiredSkill || skills.includes(requiredSkill)) {
           cssConfig.display = 'flex';
         }
